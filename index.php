b2aa172f952400b8e7d776a701c6e18bfc87d2dd
ALESSANDRO FERNANDES ROCHA JUNIOR <alessandro.6957@aluno.pr.senac.br>
2023-05-24 07:42:34 -0300

    salvar

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+</head>
+
+ <br>
+ <br>
+ <br>
+
+
+<h1>Buteco - Nunes</h1>
+    <div class="card-group">
+  <div class="card">
+  <img id="" src="imagens HAMB/60de057e-e468-4749-bab3-6c39ac1e0389.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">X-Nunes</h5>
+      <p class="card-text">Ingredientes: pão, tomate, alface, carne, maionese, molho caseiro da casa, queijo cheddar.</p>
+      <a href="pedido.php" class="btn btn-primary">Faça o seu pedido</a>
+    </div>
+  </div>
+  <div class="card">
+    <img id="lanche" src="imagens HAMB/10117675.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">X-Souza</h5>
+      <p class="card-text">O lanche favorito da galera, o X-Souza é um lanche especial do chefe recheado com: maionese, bacon, frango desfiado, carne de hambúrguer, tomate, alface, queijo cheddar e pão caseiro da casa.</p>
+      <a href="pedido.php" class="btn btn-primary">Faça o seu pedido</a>
+    </div>
+  </div>
+  <div class="card">
+    <img id="lanche" src="imagens HAMB/maxresdefault.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">X-Dieimes</h5>
+      <p class="card-text">O lanche mais caro de todos, mas o que mais alivia sua fome apresentamos o X-Dieimes um dos melhores da casa que satisfaz a fome de um Excelente Aluno, ingredientes: pão caseiro da casa, queijo cheddar, carne, tomate, alface, maionese, ketchup, mostarda, molho caseiro. </p>
+      <a href="pedido.php" class="btn btn-primary">Faça o seu pedido</a>
+    </div>
+  </div>
+</div>
+      </div>
+    </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+</html>
